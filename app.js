583de35b9405ab91fd8eb0fea625255b6c4c0113
George <george.fairs@gmail.com>
2018-09-29 15:39:06 +0000

    Progress so far: currently trying to wrap my head around kwnl

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const readline = require('readline-sync'); //Required module for taking user inputs?
 const request = require('request');
 const cheerio = require('cheerio');
+const knwl = require('knwl.js');
+
+var knwlInstance = new knwl('english');
+
 
 var emailInput = readline.question('Enter email address: '); //Take user input for email
 var atPos = emailInput.indexOf('@'); //Position of '@' character in email string
@@ -14,9 +18,13 @@ var url = 'https://www.' + urlRoot; //Affix root
 
 console.log(url); //Print full URL to console
 
+//Some URLs I've found this to work with so far:
+//https://www.ey.com/uk/en/home
+//https://www2.deloitte.com/uk/en.html
+
 request({
     method: 'GET',
-    url: url
+    url: 'https://www.canddi.com/contact'
     
 }, function getContactUrl(error, response, body)
 {
@@ -26,14 +34,29 @@ request({
     
     let $ = cheerio.load(body); //In
     
-    let contacts= $('a:contains("Contact")'); //Using cheerio we grab any a elementsconntaining the text "Contact"
+    //let contacts= $('a:contains("Contact")'); //Using cheerio we grab any a elements conntaining the text "Contact"
     
-    console.log(contacts.attr('href')); //Here we return any attribute information stored, i.e. the link address, to the console
+    //console.log(contacts.attr('href')); //Here we return any attribute information stored, i.e. the link address, to the console
     
-    let contactUrl = contacts.attr('href'); //Here we take the href target url and store it in a new variable
+    //let contactUrl = contacts.attr('href'); //Here we take the href target url and store it in a new variable
     
-    var newUrl = url + contactUrl; //And here we combine the two to get the contact page URL
-    console.log(newUrl);
+    //var newUrl = url + contactUrl; //And here we combine the two to get the contact page URL
+    //console.log(newUrl);
+    
+    let bodyText = $('body').text();
+    let bodyTextClean = bodyText.replace('.com', '.com '); //clean up --> is there a better way that'd also account for different endings, i.e. .co.uk? .net?
+    
+    //console.log(bodyTextClean);
+    
+    knwlInstance.init(bodyTextClean);
+    
+    var emails = knwlInstance.get('emails')
+    var phoneNums = knwlInstance.get('phones');
+    var addresses = knwlInstance.get('places')
+    
+    console.log(emails);
+    console.log(phoneNums);
+    console.log(addresses);
 }); 
 
 //console.log(_newUrl);
